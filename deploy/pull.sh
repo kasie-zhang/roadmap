a5b4924800b83fec4a57bb6a71afdfb7fead63db
kasie-zhang <kasie_zhang@163.com>
2022-07-20 19:34:59 +0800

    Auto deploy

--- deploy/pull.sh
+++ deploy/pull.sh
@@ -1,8 +1,12 @@
 #! /bin/bash
 
-cd /home/zhangke/workspace/nginx/webapps/roadmap;
+cd /home/zhangke/workspace/nginx/webapps/roadmap
 echo "===== Start Excute Webooks ====="
 echo "1. Start pull repo"
 git pull
 echo "2. Finish pull repo"
+echo "3. Start pull static file"
+cd /home/zhangke/workspace/nginx/files
+git pull origin master
+echo "4. Finish pull static file"
 echo "===== Finish Excute Webhooks ====="
